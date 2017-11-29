149879c6029efac02bec30b67d13592287f2898c
Arun Sharma <asharma@fb.com>
2017-11-29 14:22:08 -0800

    Add a unit test for flatc

---
+++ tests/test_flatc.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+# TODO: Duplicate code
+# Figure out how to import from flatc.py
+def get_attrs_dict(attrs):
+    attrs_dict = {x[0]: x[1] for x in attrs if len(x) == 2}
+    ret = {x[0]: None for x in attrs}
+    ret.update(attrs_dict)
+    return ret
+
+
+def test_attrs_dict():
+    assert get_attrs_dict(['a', ['b', 10], 'c']) == {'a': None, 'b': 10, 'c': None}
