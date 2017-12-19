b60b6feff20a50879b33d02bac4a7c1cacb18772
Arun Sharma <asharma@fb.com>
2017-12-19 14:08:20 -0800

    Cleanup setup.py

--- setup.py
+++ setup.py
@@ -17,36 +17,27 @@ install_requires = [
     "ply>=3.4,<4.0",
 ]
 
-tornado_requires = [
-    "tornado>=4.0,<5.0",
-    "toro>=0.6"
-]
-
 dev_requires = [
     "flake8>=2.5",
     "pytest>=2.8",
-    "sphinx-rtd-theme>=0.1.9",
-    "sphinx>=1.3",
-] + tornado_requires
+]
 
-setup(name="thriftpy",
+setup(name="flattools",
       version=version,
-      description="Pure python implementation of Apache Thrift.",
-      keywords="thrift python thriftpy",
-      author="Lx Yu",
-      author_email="i@lxyu.net",
+      description="Pure python implementation of Flatbuffers.",
+      keywords="flatbuffers python thriftpy",
+      author="Arun Sharma",
+      author_email="asharma@fb.com",
       packages=find_packages(exclude=['benchmark', 'docs', 'tests']),
-      package_data={"thriftpy": ["contrib/tracking/tracking.thrift"]},
+      package_data={"flattools": ["contrib/tracking/tracking.thrift"]},
       entry_points={},
-      url="https://thriftpy.readthedocs.org/",
+      url="https://github.com/adsharma/flattools",
       license="MIT",
       zip_safe=False,
       long_description=open("README.rst").read(),
       install_requires=install_requires,
-      tests_require=tornado_requires,
       extras_require={
           "dev": dev_requires,
-          "tornado": tornado_requires
       },
       classifiers=[
           "Topic :: Software Development",
