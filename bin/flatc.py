37e2aac2f0ddcfce9808e9d6c4c46e2ae6236cc3
Arun Sharma <asharma@fb.com>
2019-05-01 22:15:26 +0000

    Add a --templates command line arg

--- bin/flatc.py
+++ bin/flatc.py
@@ -56,48 +56,60 @@ def pre_generate_step(path):
     prefix, extension = os.path.splitext(filename)
     return (prefix, env)
 
-def generate_cpp(path, tree):
+def generate_cpp(path, tree, template=CPP_TEMPLATE):
     (prefix, env) = pre_generate_step(path)
     out_file = prefix + '_generated.h'
     with open(out_file, 'w') as target:
         setattr(tree, 'cpp_types', FBSType._VALUES_TO_CPP_TYPES)
         setattr(tree, 'get_type', partial(get_type, primitive=tree.cpp_types, module=tree))
-        target.write(env.get_template(CPP_TEMPLATE).render(tree.__dict__))
+        target.write(env.get_template(template).render(tree.__dict__))
 
-def generate_ijava(path, tree):
+def generate_ijava(path, tree, template=IJAVA_TEMPLATE):
     (prefix, env) = pre_generate_step(path)
     out_file = 'I' + prefix + '.java'
     with open(out_file, 'w') as target:
         setattr(tree, 'java_types', FBSType._VALUES_TO_JAVA_TYPES)
         setattr(tree, 'get_type', partial(get_type, primitive=tree.java_types, module=tree))
-        target.write(env.get_template(IJAVA_TEMPLATE).render(tree.__dict__))
+        target.write(env.get_template(template).render(tree.__dict__))
 
-def generate_yaml(path, tree):
+def generate_yaml(path, tree, template=YAML_TEMPLATE):
     (prefix, env) = pre_generate_step(path)
     out_file = prefix + '.yaml'
     with open(out_file, 'w') as target:
         setattr(tree, 'yaml_types', FBSType._VALUES_TO_NAMES_LOWER)
         setattr(tree, 'get_type', partial(get_type, primitive=tree.yaml_types, module=tree))
-        target.write(env.get_template(YAML_TEMPLATE).render(tree.__dict__))
+        target.write(env.get_template(template).render(tree.__dict__))
 
-def generate_py(path, tree):
+def generate_py(path, tree, templates=[PYTHON_TEMPLATE, None, None]):
     (prefix, env) = pre_generate_step(path)
     if not os.path.exists(prefix):
         os.mkdir(prefix)
         open(os.path.join(prefix, '__init__.py'), "a").close()
+    table_template, union_template, enum_template = templates
+    setattr(tree, 'python_types', FBSType._VALUES_TO_PY_TYPES)
+    setattr(tree, 'get_type', partial(get_type, primitive=tree.python_types, module=tree))
+    setattr(tree, 'camel_case', camel_case)
+    setattr(tree, "parse_types", parse_types)
     for table in tree.__fbs_meta__['tables']:
         out_file = os.path.join(prefix, table.__name__ + '.py')
         with open(out_file, 'w') as target:
-            setattr(tree, 'python_types', FBSType._VALUES_TO_PY_TYPES)
-            setattr(tree, 'get_type', partial(get_type, primitive=tree.python_types, module=tree))
             setattr(tree, 'table', table)
-            setattr(tree, 'camel_case', camel_case)
-            setattr(tree, "parse_types", parse_types)
-            target.write(env.get_template(PYTHON_TEMPLATE).render(tree.__dict__))
+            target.write(env.get_template(table_template).render(tree.__dict__))
+    for fbs_union in tree.__fbs_meta__['unions']:
+        out_file = os.path.join(prefix, fbs_union.__name__ + '.py')
+        with open(out_file, 'w') as target:
+            setattr(tree, 'fbs_union', fbs_union)
+            target.write(env.get_template(union_template).render(tree.__dict__))
+    for fbs_enum in tree.__fbs_meta__['enums']:
+        out_file = os.path.join(prefix, fbs_enum.__name__ + '.py')
+        with open(out_file, 'w') as target:
+            setattr(tree, 'fbs_enum', fbs_enum)
+            target.write(env.get_template(enum_template).render(tree.__dict__))
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--includes", action='store', nargs='+', help="Directories to search")
+    parser.add_argument("--templates", action='store', nargs='+', help="Filename to template")
     parser.add_argument("--cpp", type=bool, default=False, help="Generate C++ code")
     parser.add_argument("--ijava", type=bool, default=False, help="Generate Java interface code")
     parser.add_argument("--yaml", type=bool, default=False, help="Generate Yaml code")
@@ -114,7 +126,10 @@ def main():
         if args.yaml:
             generate_yaml(filename, load(filename))
         if args.python:
-            generate_py(filename, load(filename))
+            if args.templates:
+                generate_py(filename, load(filename), args.templates)
+            else:
+                generate_py(filename, load(filename))
 
 if __name__ == '__main__':
     main()
